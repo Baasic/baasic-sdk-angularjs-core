117bf81bd9dfa057b0bc92779d89e621f26e3a79
mzilic <mzilic@mono.hr>
2014-11-07 15:12:47 +0100

    - removed caching logic from lookup service - updated bower and package json

--- dist/baasic-angular-core.js
+++ dist/baasic-angular-core.js
@@ -392,9 +392,6 @@
     (function (angular, module, undefined) {
         "use strict";
         module.service("baasicLookupService", ["baasicApiHttp", "baasicApp", "baasicApiService", "baasicLookupRouteService", function (baasicApiHttp, baasicApp, baasicApiService, lookupRouteService) {
-            var apiKey = baasicApp.get().get_apiKey(),
-                lookupKey = "baasic-lookup-data-" + apiKey;
-
             function getResponseData(params, data) {
                 var responseData = {};
                 if (params.embed) {
@@ -413,36 +410,25 @@
                 routeService: lookupRouteService,
                 get: function (options) {
                     var deferred = baasicApiHttp.createHttpDefer();
-                    var result = JSON.parse(localStorage.getItem(lookupKey));
-                    if (result === undefined || result === null) {
-                        baasicApiHttp.get(lookupRouteService.get.expand(baasicApiService.getParams({
-                            embed: 'role,accessAction,accessSection'
-                        }))).success(function (data, status, headers, config) {
-                            localStorage.setItem(lookupKey, JSON.stringify(data));
-                            var responseData = getResponseData(options, data);
-                            deferred.resolve({
-                                data: responseData,
-                                status: status,
-                                headers: headers,
-                                config: config
-                            });
-                        }).error(function (data, status, headers, config) {
-                            deferred.reject({
-                                data: data,
-                                status: status,
-                                headers: headers,
-                                config: config
-                            });
-                        });
-                    } else {
+                    baasicApiHttp.get(lookupRouteService.get.expand(baasicApiService.getParams({
+                        embed: 'role,accessAction,accessSection'
+                    }))).success(function (data, status, headers, config) {
+                        var responseData = getResponseData(options, data);
                         deferred.resolve({
-                            data: getResponseData(options, result)
+                            data: responseData,
+                            status: status,
+                            headers: headers,
+                            config: config
                         });
-                    }
+                    }).error(function (data, status, headers, config) {
+                        deferred.reject({
+                            data: data,
+                            status: status,
+                            headers: headers,
+                            config: config
+                        });
+                    });
                     return deferred.promise;
-                },
-                reset: function () {
-                    localStorage.setItem(lookupKey, null);
                 }
             };
         }]);
--- src/services/lookupService.js
+++ src/services/lookupService.js
@@ -1,9 +1,7 @@
 ﻿(function (angular, module, undefined) {
     "use strict";
     module.service("baasicLookupService", ["baasicApiHttp", "baasicApp", "baasicApiService", "baasicLookupRouteService",
-        function (baasicApiHttp, baasicApp, baasicApiService, lookupRouteService) {
-			var apiKey = baasicApp.get().get_apiKey(), lookupKey = "baasic-lookup-data-" + apiKey;
-
+        function (baasicApiHttp, baasicApp, baasicApiService, lookupRouteService) {			
 			function getResponseData(params, data) {
 				var responseData = {};
 				if (params.embed) {
@@ -21,39 +19,28 @@
             return {
                 routeService: lookupRouteService,
                 get: function (options) {
-                    var deferred = baasicApiHttp.createHttpDefer();
-                    var result = JSON.parse(localStorage.getItem(lookupKey));
-                    if (result === undefined || result === null) {
-                        baasicApiHttp.get(lookupRouteService.get.expand(baasicApiService.getParams({
-							embed: 'role,accessAction,accessSection'
-						})))
-                            .success(function (data, status, headers, config) {
-                                localStorage.setItem(lookupKey, JSON.stringify(data));								
-								var responseData = getResponseData(options, data);								
-                                deferred.resolve({
-                                    data: responseData,
-                                    status: status,
-                                    headers: headers,
-                                    config: config
-                                });
-                            })
-                            .error(function (data, status, headers, config) {
-                                deferred.reject({
-                                    data: data,
-                                    status: status,
-                                    headers: headers,
-                                    config: config
-                                });
-                            });
-                    } else {
-                        deferred.resolve({							
-                            data: getResponseData(options, result)
-                        });
-                    }
+                    var deferred = baasicApiHttp.createHttpDefer();                                        
+					baasicApiHttp.get(lookupRouteService.get.expand(baasicApiService.getParams({
+						embed: 'role,accessAction,accessSection'
+					})))
+						.success(function (data, status, headers, config) {							
+							var responseData = getResponseData(options, data);								
+							deferred.resolve({
+								data: responseData,
+								status: status,
+								headers: headers,
+								config: config
+							});
+						})
+						.error(function (data, status, headers, config) {
+							deferred.reject({
+								data: data,
+								status: status,
+								headers: headers,
+								config: config
+							});
+						});
                     return deferred.promise;
-                },
-                reset: function () {					
-                    localStorage.setItem(lookupKey, null);
                 }
             };
         }]);
