c7f4b025edbd102bff6c811694fbc5e7e67c37a7
Dijana <dijana@mono.hr>
2015-04-03 17:36:53 +0200

    Added notes and updated service description content

--- src/services/lookupService.js
+++ src/services/lookupService.js
@@ -1,10 +1,7 @@
 ﻿/* globals module */
 /**
  * @module baasicLookupService
- * @description Baasic Lookup Service provides an easy way to consume Baasic Lookup REST API.
- * @copyright (c) 2015 Mono
- * @license MIT
- * @author Mono
+ * @description Baasic Lookup Service provides an easy way to consume Baasic Lookup REST API. In order to obtain a needed routes `baasicLookupService` uses `baasicLookupRouteService`.
 */
 (function (angular, module, undefined) {
     'use strict';
@@ -65,3 +62,12 @@ baasicLookupService.get()
             };
         }]);
 }(angular, module));
+/**
+ * @copyright (c) 2015 Mono
+ * @license MIT
+ * @author Mono
+ * @overview 
+ ***Notes:**
+ - Refer to the [REST API documentation](https://github.com/Baasic/baasic-rest-api/wiki) for detailed information about Baasic REST API end-points.
+ - All end-point objects are transformed by the associated route service.
+*/
